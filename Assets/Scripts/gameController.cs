ee603fce818bceef5f3619d44febc89df7f4366d
RakeGoose <rustem.aligozhin.99@mail.ru>
2025-03-02 13:37:34 +0500

    Grid, Enemy Logic(basic)

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+
+    public List<GameObject> wayPoints = new List<GameObject>();
+}
